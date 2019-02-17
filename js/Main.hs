8d69d244d15e191663176cb7b2088b0a29424ce4
Artyom Kazak <yom@artyom.me>
2019-02-17 15:41:39 +0100

    Embed JS (#5)
    * Embed
    
    * More JS fixes
    
    * Require eof
    
    * Paths are still wrong, but
    
    * Add homepage

--- js/Main.hs
+++ js/Main.hs
@@ -5,11 +5,15 @@ import Reflex.Dom
 import qualified Data.Text as Text
 
 main :: IO ()
-main = mainWidget $ el "div" $ do
-    t <- textArea def
-    el "pre" (dynText (Text.pack . ppr . Text.unpack <$> _textArea_value t))
+main = mainWidgetInElementById "shower-demo" $ do
+    t <- textArea $ def
+        & attributes .~ constDyn ("placeholder" =: "Paste your horrible 'Show' output here.")
+    el "pre" $
+        el "code" $
+            dynText (Text.pack . ppr . Text.unpack <$> _textArea_value t)
 
 ppr :: String -> String
+ppr "" = ""
 ppr s =
   case showerString s of
     Left e -> "Could not prettify the provided source:\n" ++ e
