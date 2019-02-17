3b7380beb6136daac2182ca4b229e1c7e487b27b
Artyom Kazak <yom@artyom.me>
2019-02-17 13:40:48 +0100

    More JS fixes

--- js/Main.hs
+++ js/Main.hs
@@ -6,10 +6,14 @@ import qualified Data.Text as Text
 
 main :: IO ()
 main = mainWidgetInElementById "shower-demo" $ do
-    t <- textArea def
-    el "pre" (dynText (Text.pack . ppr . Text.unpack <$> _textArea_value t))
+    t <- textArea $ def
+        & attributes .~ constDyn ("placeholder" =: "Paste your horrible 'Show' output here.")
+    el "pre" $
+        el "code" $
+            dynText (Text.pack . ppr . Text.unpack <$> _textArea_value t)
 
 ppr :: String -> String
+ppr "" = ""
 ppr s =
   case showerString s of
     Left e -> "Could not prettify the provided source:\n" ++ e
