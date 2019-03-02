bd1ad2f8688595d15f4270bf846b3eebc44af9e9
Vladislav Zavialov <vlad.z.4096@gmail.com>
2019-03-02 20:49:07 +0300

    Add 'printer'

--- lib/Shower.hs
+++ lib/Shower.hs
@@ -1,6 +1,7 @@
 -- | Pretty-print 'Show' output and JSON.
 module Shower
   ( shower,
+    printer,
     showerString
   ) where
 
@@ -8,6 +9,20 @@ import Data.Bifunctor (bimap)
 import Text.Megaparsec (parse, errorBundlePretty, eof)
 import Shower.Parser (pShower)
 import Shower.Printer (showerRender)
+
+-- | A drop-in replacement for @print@ that has nice layout.
+-- Use it in GHCi with @-interactive-print=Shower.printer@:
+--
+-- @
+-- ghci> :set -interactive-print=Shower.printer
+-- ghci> ([1..15], ['a'..'z'])
+-- ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+--  "abcdefghijklmnopqrstuvwxyz")
+-- @
+--
+-- NB: does not handle infinite data structures at the moment.
+printer :: Show a => a -> IO ()
+printer a = putStrLn (shower a)
 
 -- | A drop-in replacement for @show@ that has nice layout.
 -- NB: does not handle infinite data structures at the moment.
